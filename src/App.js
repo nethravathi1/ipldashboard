1cf35b0aeaca4e660ec2b5cb51a9bd956c20ddc3
nethra <nethravathib04@gmail.com>
2021-09-03 12:12:39 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// FIX1: In order to use Switch component it should be imported
+import {Route, Switch} from 'react-router-dom'
+
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+
+    <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
